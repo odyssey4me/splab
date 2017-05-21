8757fe91755c680c86a481c7dfe3fe9630e1a1a0
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2017-05-21 15:56:53 +0100

    Set apt install to be non-interactive

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Set dpkg to do non-interactive installs
+export DEBIAN_FRONTEND=noninteractive
+
 # Install Ansible
 sudo apt-get update
 sudo apt-get install -y ansible
